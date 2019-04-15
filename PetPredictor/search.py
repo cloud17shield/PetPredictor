25e317ba8e07219f08635ef7ca596db1c5186c7b
jessie <wjx961123@gmail.com>
2019-04-15 14:35:49 +0800

    add producer & consumer for csv

--- PetPredictor/search.py
+++ PetPredictor/search.py
@@ -27,46 +27,43 @@ def search(request):
 
         # retrive image from front-end
         myFile = request.FILES.get("myfile", None)
-        if myFile:
+        if myFile and request.POST['selection'] == 'byImage':
             print('upload successfully')
-            message = "upload successfully"
-            # 打开特定的文件进行二进制的写操作
             # use it if need to store image
             rnd_file_name = ''.join(random.sample(string.ascii_letters + string.digits, 10)) + '.' + \
                             myFile.name.split('.')[1]
-            print('random name', rnd_file_name)
             destination = open(os.path.join("static", rnd_file_name), 'wb+')
             # copy the uploaded file to hdfs
             for chunk in myFile.chunks():
                 destination.write(chunk)
             destination.close()
-            # copy the uploaded file to hdfs
             os.system('hdfs dfs -copyFromLocal /home/hduser/UI/PetPredictor/static/' + rnd_file_name + ' /images')
+            
             # - default kafka topic to write to
-            topic_name = 'fun'
-
+            input_topic_name = 'input'
+            output_topic_name = 'output'
             # - default kafka broker location
             kafka_broker = 'gpu17:9092'
 
             try:
-                # price = json.dumps(getQuotes(symbol))
-                # - instantiate a simple kafka producer
                 print("creating producer")
-                producer = KafkaProducer(
-                    bootstrap_servers=kafka_broker
-                )
+                producer = KafkaProducer(bootstrap_servers=kafka_broker)
                 timestamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%dT%H:%MZ')
                 IMG_url = 'hdfs:///images/' + rnd_file_name
                 payload = ('[{"IMG_url":%s,"Produce_Time":"%s"}]' % (
                 IMG_url, timestamp)).encode('utf-8')
-                producer.send(topic_name, key=rnd_file_name.encode('utf-8'), value=payload)
+                producer.send(input_topic_name, key=rnd_file_name.encode('utf-8'), value=payload)
+                
+                print (rnd_file_name.encode('utf-8'))
+
+                consumer = KafkaConsumer(bootstrap_servers=kafka_broker)
+                consumer.assign([TopicPartition(output_topic_name,0)])
+            
             except KafkaTimeoutError as timeout_error:
                 print("time out error!")
             except Exception:
                 print("other kafka exception!")
 
-            consumer = KafkaConsumer(bootstrap_servers=kafka_broker)
-            consumer.assign([TopicPartition(topic_name,0)])
 
             for msg in consumer:
                 print (msg)
@@ -74,92 +71,111 @@ def search(request):
                     print (msg.value)
                     break
 
-        else:
-            message = ('no image uploaded')
+            prediction = 'The prediction is' + str(msg.value)
+            return HttpResponse(prediction)
 
-        # retrive attributes from front-end
+        elif request.POST['selection'] == 'byValues': 
+            petType = request.POST['petType']
 
-        petType = request.POST['petType']
-        # name = request.POST['name']
+            if request.POST['age']:
+                age = int(request.POST['age'])
+            else:
+                age = 0
+            gender = int(request.POST['gender'])
+            color1 = int(request.POST['color1'])
+            color2 = int(request.POST['color2'])
+            color3 = int(request.POST['color3'])
+            maturitySize = int(request.POST['maturitySize'])
+            furLength = int(request.POST['furLength'])
+            vaccinated = int(request.POST['vaccinated'])
+            dewormed = int(request.POST['dewormed'])
+            sterilized = int(request.POST['sterilized'])
+            health = int(request.POST['health'])
 
-        if request.POST['age']:
-            age = int(request.POST['age'])
-        else:
-            age = 0
+            if request.POST['quantity']:
+                quantity = int(request.POST['quantity'])
+            else:
+                quantity = 1
 
-        gender = int(request.POST['gender'])
-        color1 = int(request.POST['color1'])
-        color2 = int(request.POST['color2'])
-        color3 = int(request.POST['color3'])
-        maturitySize = int(request.POST['maturitySize'])
-        furLength = int(request.POST['furLength'])
-        vaccinated = int(request.POST['vaccinated'])
-        dewormed = int(request.POST['dewormed'])
-        sterilized = int(request.POST['sterilized'])
-        health = int(request.POST['health'])
+            if request.POST['fee']:
+                fee = int(request.POST['fee'])
+            else:
+                fee = 0
 
-        if request.POST['quantity']:
-            quantity = int(request.POST['quantity'])
-        else:
-            quantity = 1
+            state = int(request.POST['state'])
 
-        if request.POST['fee']:
-            fee = int(request.POST['fee'])
-        else:
-            fee = 0
+            if request.POST['videoAmt']:
+                videoAmt = int(request.POST['videoAmt'])
+            else:
+                videoAmt = 0
 
-        state = int(request.POST['state'])
+            if request.POST['photoAmt']:
+                photoAmt = int(request.POST['photoAmt'])
+            else:
+                photoAmt = 0
 
-        if request.POST['videoAmt']:
-            videoAmt = int(request.POST['videoAmt'])
-        else:
-            videoAmt = 0
+            # deal with breed, if no match then put the breed value as 0
+            df = pd.read_csv('PetPredictor/breed_labels.csv', header=0, sep=',')
 
-        if request.POST['photoAmt']:
-            photoAmt = int(request.POST['photoAmt'])
-        else:
-            photoAmt = 0
-        # description = request.POST['description']
+            if request.POST['breed1'] == '':
+                breed1 = 0
+            else:
+                text = request.POST['breed1']
+                breed1 = 0
+                for index, row in df.iterrows():
+                    if row['BreedName'] == text:
+                        breed1 = int(row['BreedID'])
+                        break
 
-        # deal with breed, if no match then put the breed value as 0
+            if request.POST['breed2'] == '':
+                breed2 = 0
+            else:
+                text = request.POST['breed2']
+                breed2 = 0
+                for index, row in df.iterrows():
+                    if row['BreedName'] == text:
+                        breed2 = int(row['BreedID'])
+                        break
 
-        df = pd.read_csv('PetPredictor/breed_labels.csv', header=0, sep=',')
+            rnd_key = random.sample(string.ascii_letters + string.digits, 10)
+            # - default kafka topic to write to
+            input_topic_name = 'input'
+            output_topic_name = 'output'
+            # - default kafka broker location
+            kafka_broker = 'gpu17:9092'
+            
+            payload = str(petType)+','+str(age)+','+str(breed1)+','+str(breed2)+','+str(gender)+','+str(color1)+','+str(color2)+','+str(color3)+','+str(maturitySize)+','+str(furLength)+','+str(vaccinated)+','+str(dewormed)+','+str(sterilized)+','+str(health)+','+str(quantity)+','+str(fee)+','+str(videoAmt)+','+str(photoAmt)
 
-        if request.POST['breed1'] == '':
-            breed1 = 0
-        else:
-            text = request.POST['breed1']
-            breed1 = 0
-            for index, row in df.iterrows():
-                if row['BreedName'] == text:
-                    breed1 = int(row['BreedID'])
+            try:
+                print("creating producer")
+                producer = KafkaProducer(bootstrap_servers=kafka_broker)
+                producer.send(input_topic_name, key=rnd_key.encode('utf-8'), value=payload.encode('utf-8'))
+
+                print (rnd_key.encode('utf-8'))
+
+                consumer = KafkaConsumer(bootstrap_servers=kafka_broker)
+                consumer.assign([TopicPartition(output_topic_name,0)])
+            
+            except KafkaTimeoutError as timeout_error:
+                print("time out error!")
+            except Exception:
+                print("other kafka exception!")
+
+
+            for msg in consumer:
+                print (msg)
+                if msg.key == rnd_key.encode('utf-8'):
+                    print (msg.value)
                     break
 
-        if request.POST['breed2'] == '':
-            breed2 = 0
-        else:
-            text = request.POST['breed2']
-            breed2 = 0
-            for index, row in df.iterrows():
-                if row['BreedName'] == text:
-                    breed2 = int(row['BreedID'])
-                    break
+            prediction = 'The prediction is' + str(msg.value)
+            return HttpResponse(prediction)
 
-        print("_____________________")
-        print(petType, age, gender, color1, color2, color3, maturitySize, furLength, vaccinated, dewormed, sterilized,
-              health, quantity, fee, state, videoAmt, photoAmt)
-        print(breed1, breed2)
-        # message = "The prediction is ...."
 
-        # make prediction
+            print("_____________________")
+            print(petType, age, gender, color1, color2, color3, maturitySize, furLength, vaccinated, dewormed, sterilized,
+                  health, quantity, fee, state, videoAmt, photoAmt)
+            print(breed1, breed2)
+            print("_____________________")
 
-        if request.POST['selection'] == 'byImage':
-            message = 'predicted by image'
-        elif request.POST['selection'] == 'byValues':
-            message = 'predicted by values'
-
-        # return prediction back to front end
-        ctx['rlt'] = message
-    # ctx['rlt'] = request.POST['name']
-
-    return render(request, "search_form.html", ctx)
+        return render(request, "search_form.html", ctx)
