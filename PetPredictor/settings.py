88ab83dbb7cbba7372c9142fa728495b3f3de464
jessie <wjx961123@gmail.com>
2019-04-16 15:46:06 +0800

    css

--- PetPredictor/settings.py
+++ PetPredictor/settings.py
@@ -123,3 +123,13 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static'),
 ]
 MEDIA_ROOT=os.path.join(BASE_DIR,'static/media')
+
+STATIC_URL = '/static/'
+HERE = os.path.dirname(os.path.abspath(__file__))
+HERE = os.path.join(HERE, '../')
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(HERE, 'static/'),
+)
